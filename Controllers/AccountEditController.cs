fed1d2a18f4165798cbb8f024b364c79b6cce3d9
nadamohsen15 <nadamohsen15@gmail.com>
2024-03-24 22:39:42 +0200

    push change pass

---
+++ Controllers/AccountEditController.cs
@@ -0,0 +1,52 @@
+﻿using Exam_System.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exam_System.Controllers
+{
+    public class AccountEditController : Controller
+    {
+
+        ExaminationContext db = new ExaminationContext();
+        public IActionResult ChangepassStd(int stdID)
+        {
+            var model = db.Students.FirstOrDefault(a => a.StudentId == stdID);
+
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult ChangepassStd(string StudentPassword)
+        {
+            var userIdClaim = HttpContext.User.FindFirst("UserId");
+            int userId = int.Parse(userIdClaim.Value);
+            var model = db.Students.FirstOrDefault(a => a.StudentId == userId);
+            model.StudentPassword = StudentPassword;
+
+            db.Students.Update(model);
+            db.SaveChanges();
+
+            return RedirectToAction("ShowCourses", "HomePage", new { id = userId });
+        }
+        public IActionResult changpassinstructor()
+        {
+            var userIdClaim = HttpContext.User.FindFirst("UserId");
+            int userId = int.Parse(userIdClaim.Value);
+            var model = db.Instructors.FirstOrDefault(a => a.InstructorId == userId);
+
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult changpassinstructor(string InstructorPassword)
+        {
+            var userIdClaim = HttpContext.User.FindFirst("UserId");
+            int userId = int.Parse(userIdClaim.Value);
+            var model = db.Instructors.FirstOrDefault(a => a.InstructorId == userId);
+            model.InstructorPassword = InstructorPassword;
+
+            db.Instructors.Update(model);
+            db.SaveChanges();
+
+            return RedirectToAction("index", "instructor");
+        }
+
+    }
+}
