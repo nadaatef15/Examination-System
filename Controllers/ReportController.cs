ede8641c5bd3231bc87bf84c9fe8f1d7490bbec9
nadamohsen15 <nadamohsen15@gmail.com>
2024-03-25 10:51:15 +0200

    report design

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Exam_System.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult showReport()
+        {
+            return View();
+        }
+    }
+}
